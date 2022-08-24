c0819c473126c4cf19e5c69635d09f92a72987a0
Peter Hull <peter@sixfeetup.com>
2022-08-24 13:01:38 -0700

    Removing channels bits so Django can run on asgi alone

--- {{cookiecutter.project_slug}}/config/asgi.py
+++ {{cookiecutter.project_slug}}/config/asgi.py
@@ -13,12 +13,8 @@ from pathlib import Path
 
 import os
 
-from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    # Just HTTP for now. (We can add other protocols later.)
-})
+application = get_asgi_application()
