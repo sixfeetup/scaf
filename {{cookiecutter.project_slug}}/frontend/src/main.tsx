cf3b32b5bd49f1f5d952facd0968b24f02d334ed
sedatbasar <sedat@whitespectre.com>
2023-01-06 20:04:12 +0300

    adds react query

--- {{cookiecutter.project_slug}}/frontend/src/main.tsx
+++ {{cookiecutter.project_slug}}/frontend/src/main.tsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: Number.POSITIVE_INFINITY,
+			retry: 1
+		}
+	}
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider> 
 	</React.StrictMode>
 )
--- {{cookiecutter.project_slug}}/frontend/src/pages/Home.tsx
+++ {{cookiecutter.project_slug}}/frontend/src/pages/Home.tsx
@@ -1,13 +1,20 @@
+import { useQuery } from '@tanstack/react-query'
+
 import Card from 'components/card/Card'
+
 import styles from './Home.module.css'
+import { fetchMe } from './Home.queries'
 const Home = () => {
+	const {data, isLoading, error} = useQuery(['you'], fetchMe)
+	console.log(isLoading, data, error)
 	return (
 		<div className={styles.Home}>
+			{isLoading ? <div>Loading...</div> :
 			<Card
 				heading='Cookie Cutter'
 				body='🚀 Vite + React + Typescript 🤘 & Eslint 🔥+ Prettier'
 				footer='This is cookie cutter template'
-			/>
+			/>}
 		</div>
 	)
 }
