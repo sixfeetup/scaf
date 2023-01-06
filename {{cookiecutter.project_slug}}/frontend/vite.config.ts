0f54e0262fba6b55a6aa9d7be70c97d2b2634f0d
sedatbasar <sedat@whitespectre.com>
2023-01-06 13:46:53 +0300

    put default port for frontend to be 3000

--- {{cookiecutter.project_slug}}/frontend/vite.config.ts
+++ {{cookiecutter.project_slug}}/frontend/vite.config.ts
@@ -4,5 +4,8 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()]
+	plugins: [react(), tsconfigPaths()],
+	server: {
+		port: 3000
+	}
 })
