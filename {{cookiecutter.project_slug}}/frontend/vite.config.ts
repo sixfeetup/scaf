e0d27f0b5d11730a0615c5bb80cdf5573aea9ce6
sedatbasar <sedat@whitespectre.com>
2023-01-08 11:50:53 +0300

    use static version of the packages

--- {{cookiecutter.project_slug}}/frontend/vite.config.ts
+++ {{cookiecutter.project_slug}}/frontend/vite.config.ts
@@ -2,24 +2,20 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-
 export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') }
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') }
 
-  return defineConfig({
-    plugins: [
-      react(),
-      tsconfigPaths(),
-    ],
-    server: {
+	return defineConfig({
+		plugins: [react(), tsconfigPaths()],
+		server: {
 			host: '0.0.0.0',
-      port: 3000,
-      proxy: {
-        '/api': {
-          target: process.env.VITE_PROXY_TARGET || 'http://django:8000',
-          changeOrigin: false,
-        },
-      },
-    },
-  })
+			port: 3000,
+			proxy: {
+				'/api': {
+					target: process.env.VITE_PROXY_TARGET || 'http://django:8000',
+					changeOrigin: false
+				}
+			}
+		}
+	})
 }
