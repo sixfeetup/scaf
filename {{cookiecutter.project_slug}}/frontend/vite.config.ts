e37829a4921fff557a1ceac4a9e2f198daf73b7a
sedatbasar <sedat@whitespectre.com>
2023-02-16 15:11:03 +0300

    Adds vite plugin checker and vite plugin svgr

--- {{cookiecutter.project_slug}}/frontend/vite.config.ts
+++ {{cookiecutter.project_slug}}/frontend/vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import checker from 'vite-plugin-checker'
+import svgr from 'vite-plugin-svgr'
 
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') }
 
   return defineConfig({
-    plugins: [react(), tsconfigPaths()],
+    plugins: [checker({ typescript: true }), svgr(), react(), tsconfigPaths()],
     server: {
       host: '0.0.0.0',
       port: 3000,
