633ec214864106ebd033afc30bf7ee38003e67eb
Roché Compaan <roche@upfrontsoftware.co.za>
2024-06-29 15:23:15 +0200

    fix: move backend config to root module

--- {{cookiecutter.project_slug}}/terraform/modules/base/backend.tf
+++ {{cookiecutter.project_slug}}/terraform/modules/base/backend.tf
@@ -7,19 +7,3 @@ provider "aws" {
   region = "us-east-1"
   alias  = "us_east_1"
 }
-
-# each environment has has a unique key to isolate state per environment
-# to isolate environments completely, it is recommended to have a separate AWS
-# sub-account per envionment
-terraform {
-  required_version = ">= 1.4"
-
-  backend "s3" {
-    region         = var.aws_region
-    bucket         = "${var.app_name}-terraform-state"
-    key            = "${var.app_name}-${var.environment}.cluster.json"
-    encrypt        = true
-    dynamodb_table = "${var.app_name}-terraform-state"
-  }
-}
-
