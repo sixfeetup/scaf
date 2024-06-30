9d56dc6a8e99b32d692194eb791e8faab99b450b
Roché Compaan <roche@upfrontsoftware.co.za>
2024-06-30 14:25:37 +0200

    docs: set allowed ips for cluster access

--- {{cookiecutter.project_slug}}/terraform/staging/cluster.tf
+++ {{cookiecutter.project_slug}}/terraform/staging/cluster.tf
@@ -15,6 +15,9 @@ module "cluster" {
     # changes, eg:
     # ami_id = "ami-09d22b42af049d453"
 
-module "base" {
-  source = "../modules/base"
+    # NB!: limit kubectl_allowed_ips and talos_allowed_ips to a set of trusted
+    # public ip addresses. Both variables are comma separated lists of ips.
+    # kubectl_allowed_ips = "10.0.0.1/32,10.0.0.2/32"
+    # talos_allowed_ips = "10.0.0.1/32,10.0.0.2/32"
+  }
 }
