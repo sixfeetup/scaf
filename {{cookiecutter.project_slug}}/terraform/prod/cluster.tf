1be16d75c9937b9ab4b46abb07e2db37cbe18055
Roché Compaan <roche@upfrontsoftware.co.za>
2024-06-30 14:16:47 +0200

    chore: set control plane vars for each environment

--- {{cookiecutter.project_slug}}/terraform/prod/cluster.tf
+++ {{cookiecutter.project_slug}}/terraform/prod/cluster.tf
@@ -7,7 +7,13 @@ module "cluster" {
   cluster_domain_name    = "k8s.prod.{{ cookiecutter.domain_name }}"
   argocd_domain_name     = "argocd.prod.{{ cookiecutter.domain_name }}"
   prometheus_domain_name = "prometheus.prod.{{ cookiecutter.domain_name }}"
-}
+  control_plane = {
+    # 2 vCPUs, 4 GiB RAM, $0.0376 per Hour
+    instance_type = "t3a.medium"
+    num_instances = 3
+    # NB!: set ami_id to prevent instance recreation when the latest ami
+    # changes, eg:
+    # ami_id = "ami-09d22b42af049d453"
 
 module "base" {
   source = "../modules/base"
--- {{cookiecutter.project_slug}}/terraform/sandbox/cluster.tf
+++ {{cookiecutter.project_slug}}/terraform/sandbox/cluster.tf
@@ -1,6 +1,5 @@
 module "cluster" {
-  source = "../modules/base"
-
+  source                 = "../modules/base"
   environment            = "sandbox"
   cluster_name           = "{{ cookiecutter.project_dash }}-sandbox"
   domain_name            = "sandbox.{{ cookiecutter.domain_name }}"
@@ -9,14 +8,11 @@ module "cluster" {
   argocd_domain_name     = "argocd.sandbox.{{ cookiecutter.domain_name }}"
   prometheus_domain_name = "prometheus.sandbox.{{ cookiecutter.domain_name }}"
   control_plane = {
-    # curl -sL https://github.com/siderolabs/talos/releases/download/v1.7.4/cloud-images.json | jq -r '.[] | select(.cloud == "aws" and .region == "us-east-1" and .arch == "amd64")'
-    ami_id = "ami-04be44740c9604fa2"
     # 2 vCPUs, 2 GiB RAM, $0.0188 per Hour
     instance_type = "t3a.small"
     num_instances = 3
-  }
-}
+    # NB!: set ami_id to prevent instance recreation when the latest ami
+    # changes, eg:
+    # ami_id = "ami-09d22b42af049d453"
 
-module "base" {
-  source = "../modules/base"
 }
