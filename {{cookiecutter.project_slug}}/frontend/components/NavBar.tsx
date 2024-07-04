22a0ecb058095db39980a62f3a41d757374db178
sedatbasar <sedat@whitespectre.com>
2024-07-04 12:38:56 +0300

    Add prettier-plugin-tailwindcss

--- {{cookiecutter.project_slug}}/frontend/components/NavBar.tsx
+++ {{cookiecutter.project_slug}}/frontend/components/NavBar.tsx
@@ -1,11 +1,11 @@
-import Image from 'next/image';
+import Image from 'next/image'
 import Link from 'next/link'
 
 const NavBar = () => {
   return (
     <nav className='bg-gray-800 p-4'>
       <div className='container mx-auto'>
-        <div className='flex gap-4 items-center text-white text-lg font-bold'>
+        <div className='flex items-center gap-4 text-lg font-bold text-white'>
           <Image src='/scaf-logo.png' alt='Scaf Logo' width={60} height={75} />
           <Link href='/'>Home</Link>
           <Link href='/about'>About</Link>
