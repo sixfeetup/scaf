064d14ded39659f453cc02caf74a4ffdd08d9a09
sedatbasar <sedat@whitespectre.com>
2024-07-04 14:57:48 +0300

    Run prettier

--- {{cookiecutter.project_slug}}/frontend/__tests__/about.test.tsx
+++ {{cookiecutter.project_slug}}/frontend/__tests__/about.test.tsx
@@ -1,7 +1,7 @@
 import { expect, test } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import AboutPage from '../pages/about'
- 
+
 test('AboutPage', () => {
   render(<AboutPage />)
   expect(screen.getByRole('heading', { level: 1, name: 'About Page' })).toBeDefined()
