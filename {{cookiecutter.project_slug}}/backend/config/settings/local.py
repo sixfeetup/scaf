dc458c3baddc14248db16bcfcdabb4749e8adb8d
sedatbasar <43781806+sedatbasar@users.noreply.github.com>
2024-07-08 19:32:03 +0300

    Update {{cookiecutter.project_slug}}/backend/config/settings/local.py
    Co-authored-by: Roché Compaan <roche@upfrontsoftware.co.za>

--- {{cookiecutter.project_slug}}/backend/config/settings/local.py
+++ {{cookiecutter.project_slug}}/backend/config/settings/local.py
@@ -13,7 +13,7 @@ SECRET_KEY = env(
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env(
     "DJANGO_ALLOWED_HOSTS",
-    default=["backend"],
+    default=["backend", "localhost", "127.0.0.1"],
 )
 
 # CACHES
