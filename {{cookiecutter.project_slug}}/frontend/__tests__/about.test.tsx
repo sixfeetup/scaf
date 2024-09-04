4a2a5b254891fd4b1688499d17240aa93356ce81
sedatbasar <43781806+sedatbasar@users.noreply.github.com>
2024-09-04 21:31:03 +0300

    fix: Fixes frontend tests (#363)
    **QA Steps:**
    1. Create a scaf project
    2. Cd into frontend folder
    3. run `npm run test` and expect to see success (make sure `npm i` is
    run to install packages)
    
    ---------
    
    Co-authored-by: sedatbasar <sedat@whitespectre.com>

--- {{cookiecutter.project_slug}}/frontend/__tests__/about.test.tsx
+++ {{cookiecutter.project_slug}}/frontend/__tests__/about.test.tsx
@@ -1,8 +1,23 @@
 import { expect, test } from 'vitest'
-import { render, screen } from '@testing-library/react'
 import AboutPage from '../pages/about'
+import { GET_ME } from '@/pages'
+import { render, screen } from '@/utils/test-utils'
 
-test('AboutPage', () => {
-  render(<AboutPage />)
-  expect(screen.getByRole('heading', { level: 1, name: 'About Page' })).toBeDefined()
+const mocks = [
+  {
+    request: {
+      query: GET_ME
+    },
+    result: {
+      data: {
+        me: { id: '1', name: 'John Doe' }
+      }
+    }
+  }
+]
+
+test('AboutPage', async () => {
+  render(<AboutPage />, { mocks })
+  expect(await screen.findByRole('heading', { level: 1, name: 'About Page' })).toBeInTheDocument()
+  expect(await screen.findByText('John Doe')).toBeInTheDocument()
 })
--- {{cookiecutter.project_slug}}/frontend/pages/about.tsx
+++ {{cookiecutter.project_slug}}/frontend/pages/about.tsx
@@ -1,8 +1,8 @@
 import { useQuery } from '@apollo/client'
 
-import { addApolloState, initializeApollo } from '@/lib/apolloClient'
-
 import { GET_ME } from '.'
+
+import { addApolloState, initializeApollo } from '@/lib/apolloClient'
 
 export default function About() {
   const { loading, error, data } = useQuery(GET_ME)
