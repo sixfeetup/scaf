141dc16f5b461a4fc16a63a273cfe12b4c569a5e
Roché Compaan <roche@upfrontsoftware.co.za>
2024-12-10 13:35:53 +0200

    feat: add private ips to output

--- {{cookiecutter.project_slug}}/terraform/modules/base/outputs.tf
+++ {{cookiecutter.project_slug}}/terraform/modules/base/outputs.tf
@@ -28,3 +28,8 @@ output "control_plane_nodes_public_ips" {
   description = "The public ip addresses of the control plane nodes."
   value       = join(",", module.control_plane_nodes.*.public_ip)
 }
+
+output "control_plane_nodes_private_ips" {
+  description = "The private ip addresses of the control plane nodes."
+  value       = join(",", module.control_plane_nodes.*.private_ip)
+}
