e8578260b54102091b2692a18e6fe2b2f7ab2ada
Muhammad Rehan <rehan@sixfeetup.com>
2024-12-30 16:14:49 +0500

    Move project template to new repo

--- {{cookiecutter.project_slug}}/docs/project-overview.md
+++
@@ -1,3 +0,0 @@
-# :telescope: Project overview
-
-TODO: Describe why this project is needed and what it's about
--- {{cookiecutter.project_slug}}/terraform/modules/base/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  common_tags = merge(var.tags, {
-    automation          = "terraform"
-    "automation.config" = join(".", [var.app_name, var.environment])
-    application         = var.app_name
-    environment         = var.environment
-  })
-}
