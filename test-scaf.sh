29d78d9406994dda2d06bd1be155e51e393ee1a1
abosio <abosio@sixfeetup.com>
2025-03-06 15:29:39 -0500

    feat: remove git branch from test script refs #474

--- test-scaf.sh
+++ test-scaf.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
 # Default values
-BRANCH_NAME=""
 OUTPUT_FOLDER="/tmp/scaf-test"
 TEST_CONFIG="./test-configs/nextjs-django-github.yaml"
 
 # Usage function to display help message
 usage() {
-  echo "Usage: $0 [-b <branch_name>] [-o <output_folder>] [-c <config_file>] [-h]"
-  echo "  -b <branch_name>    Optional: Specify the branch to test (default is local checkout)"
+  echo "Usage: $0 [-o <output_folder>] [-c <config_file>] [-h]"
   echo "  -o <output_folder>  Optional: Specify the output folder (default is /tmp/scaf-test)"
   echo "  -c <config_file>    Optional: Specify the config file (default is $TEST_CONFIG)"
   echo "  -h                  Show this help message"
@@ -16,11 +14,8 @@ usage() {
 }
 
 # Parse command-line arguments
-while getopts ":b:o:c:h" opt; do
+while getopts ":o:c:h" opt; do
   case ${opt} in
-    b )
-      BRANCH_NAME=$OPTARG
-      ;;
     o )
       OUTPUT_FOLDER=$OPTARG
       ;;
@@ -53,13 +48,6 @@ fi
 # Clean the output folder.
 rm -rf "$OUTPUT_FOLDER"
 
-# Check if a branch name was provided.
-if [[ -n "$BRANCH_NAME" ]]; then
-  # Test with the specified branch.
-  cookiecutter "$SCAF_ROOT" --checkout "$BRANCH_NAME" --no-input --config-file "$TEST_CONFIG" -o "$OUTPUT_FOLDER" -v
-  echo "Test completed using branch '$BRANCH_NAME'. Check the $OUTPUT_FOLDER for the generated project."
-else
-  # Use local checkout.
-  cookiecutter "$SCAF_ROOT" --no-input --config-file "$TEST_CONFIG" -o "$OUTPUT_FOLDER" -v
-  echo "Test completed using the local checkout. Check the $OUTPUT_FOLDER for the generated project."
-fi
+# Use local checkout.
+cookiecutter "$SCAF_ROOT" --no-input --config-file "$TEST_CONFIG" -o "$OUTPUT_FOLDER" -v
+echo "Test completed using the local checkout. Check the $OUTPUT_FOLDER for the generated project."
