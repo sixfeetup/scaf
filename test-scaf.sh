85f4608610215e8b0693c6de6022728c4deb348d
abosio <abosio@sixfeetup.com>
2025-03-12 13:54:44 -0400

    feat: update test script to work with copier refs #474

--- test-scaf.sh
+++ test-scaf.sh
@@ -2,25 +2,29 @@
 
 # Default values
 OUTPUT_FOLDER="/tmp/scaf-test"
-TEST_CONFIG="./test-configs/nextjs-django-github.yaml"
+TEST_DATA=""
 
 # Usage function to display help message
 usage() {
-  echo "Usage: $0 [-o <output_folder>] [-c <config_file>] [-h]"
+  echo "Usage: $0 -t <template_src> [-o <output_folder>] [-a <data_file>] [-h]"
+  echo "  -t <template_src>   Required: Specify the source folder for the template"
   echo "  -o <output_folder>  Optional: Specify the output folder (default is /tmp/scaf-test)"
-  echo "  -c <config_file>    Optional: Specify the config file (default is $TEST_CONFIG)"
+  echo "  -d <data_file>      Optional: Specify a preset answers data file"
   echo "  -h                  Show this help message"
   exit 0
 }
 
 # Parse command-line arguments
-while getopts ":o:c:h" opt; do
+while getopts ":t:o:d:h" opt; do
   case ${opt} in
+    t )
+      TEMPLATE_FOLDER=$OPTARG
+      ;;
     o )
       OUTPUT_FOLDER=$OPTARG
       ;;
-    c )
-      TEST_CONFIG=$OPTARG
+    d )
+      TEST_DATA=$OPTARG
       ;;
     h )
       usage
@@ -39,9 +43,9 @@ done
 # Dynamically set the SCAF_ROOT to the directory where this script is located.
 SCAF_ROOT=$(dirname "$(realpath "$0")")
 
-# Ensure the test configuration file exists.
-if [[ ! -f "$TEST_CONFIG" ]]; then
-  echo "Test config file ($TEST_CONFIG) not found!"
+# Ensure the test configuration file exists if provided
+if [[ -n "$TEST_DATA" && ! -f "$TEST_DATA" ]]; then
+  echo "Test config file ($TEST_DATA) not found!"
   exit 1
 fi
 
@@ -49,5 +53,9 @@ fi
 rm -rf "$OUTPUT_FOLDER"
 
 # Use local checkout.
-cookiecutter "$SCAF_ROOT" --no-input --config-file "$TEST_CONFIG" -o "$OUTPUT_FOLDER" -v
-echo "Test completed using the local checkout. Check the $OUTPUT_FOLDER for the generated project."
+if [[ -n "$TEST_DATA" ]]; then
+  copier copy -r HEAD --trust "$TEMPLATE_FOLDER" "$OUTPUT_FOLDER" --data-file "$TEST_DATA" -q
+else
+  copier copy -r HEAD --trust "$TEMPLATE_FOLDER" "$OUTPUT_FOLDER"
+fi
+echo "Test completed using the local checkouts. Check the $OUTPUT_FOLDER for the generated project."
